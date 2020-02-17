3605b2e92053d1b4b1cbe3beefd9b160494dfbcd
belchicky <kostjab@gmail.com>
2020-02-17 17:02:41 +0100

    Initial Commit

---
+++ Course2Week4Task4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Course2Week4Task4
+//
+//  Created by Konstantins Belcickis on 17/02/2020.
+//  Copyright © 2020 Konstantins Belcickis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
